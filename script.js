4d0fd630dd4814934d2f5174cc63b972f48b95e6
LilMargaryan <51162523+LilMargaryan@users.noreply.github.com>
2019-06-23 18:54:38 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,124 @@
 
-//! Setup function fires automatically
 function setup() {
-
+    guyn = "#006600";
     var socket = io();
 
-    var side = 30;
-
-    var matrix = [];
-
-    //! Getting DOM objects (HTML elements)
+    var side = 20;
+    matrix = [];
     let grassCountElement = document.getElementById('grassCount');
     let grassEaterCountElement = document.getElementById('grassEaterCount');
-
-    //! adding socket listener on "data" <-- name, after that fire 'drawCreatures' function 
+    let GishatichCountElement = document.getElementById('gishatichcounter');
+    let bnapahpanCountElement = document.getElementById('buys');
+    let kendanapahpanCountElement = document.getElementById('kendani');
+    let balansaCountElement = document.getElementById('balans');
+    let guyner = document.getElementById('guyner');
 
     socket.on("data", drawCreatures);
-
     function drawCreatures(data) {
         //! after getting data pass it to matrix variable
+
         matrix = data.matrix;
         grassCountElement.innerText = data.grassCounter;
         grassEaterCountElement.innerText = data.grassEaterCounter;
+        GishatichCountElement.innerText = data.gishatichcounter;
+        bnapahpanCountElement.innerText = data.bnapahpanhashiv;
+        kendanapahpanCountElement.innerText = data.kendanapahpanhashiv;
+        balansaCountElement.innerText = data.balansapahpanhashiv;
         //! Every time it creates new Canvas woth new matrix size
         createCanvas(matrix[0].length * side, matrix.length * side)
+        
+
         //! clearing background by setting it to new grey color
-        background('#acacac');
+        // background('#acacac');
         //! Draw grassCount and grassEaterCount to HTML (use DOM objects to update information, yes, and use .innerText <- function)
 
         //! Drawing and coloring RECTs
         for (var i = 0; i < matrix.length; i++) {
             for (var j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 1) {
-                    fill("green");
+                    fill(guyn);
                     rect(j * side, i * side, side, side);
                 } else if (matrix[i][j] == 2) {
-                    fill("orange");
+                    fill("yellow");
                     rect(j * side, i * side, side, side);
                 } else if (matrix[i][j] == 0) {
                     fill('#acacac');
                     rect(j * side, i * side, side, side);
                 } else if (matrix[i][j] == 3) {
-                    fill('red');
+                    fill('brown');
                     rect(j * side, i * side, side, side);
                 } else if (matrix[i][j] == 4) {
-                    fill('blue');
+                    fill('white');
                     rect(j * side, i * side, side, side);
                 } else if (matrix[i][j] == 5) {
-                    fill('yellow');
+                    fill('blue');
                     rect(j * side, i * side, side, side);
                 }
-            }
+                else if (matrix[i][j] == 6) {
+                    fill('#ff3333');
+                    rect(j * side, i * side, side, side);
+                }
+                else if (matrix[i][j] == 7) {
+                    fill('#cc0000');
+                    rect(j * side, i * side, side, side);
+                }
+           
+    
+   
+
+    // var doc = document.getElementById("background");
+    if (data.count < 10 ) {
+        guyner.innerText = "Spring"
+        document.body.style.backgroundColor = "#006600";
+        if (matrix[i][j] == 1) {
+            fill("#006600");
+            rect(j * side, i * side, side, side);
+        }
+       
+    }
+    if (data.count >=10 && data.count<20) {
+        guyner.innerText = "Summer"
+        
+        document.body.style.backgroundColor = "#009933";
+        if (matrix[i][j] == 1) {
+            fill("#009933");
+            rect(j * side, i * side, side, side);
         }
     }
+    if (data.count >=20  && data.count<30) {
+        guyner.innerText = "Autumn"
+        
+       document.body.style.backgroundColor = "#cc9900";
+
+        if (matrix[i][j] == 1) {
+            fill("#cc9900");
+            rect(j * side, i * side, side, side);
+        }
+    }
+    if (data.count >= 30 && data.count<40) {
+        guyner.innerText = "Winter"
+     
+         document.body.style.backgroundColor = "#ffffcc";
+        if (matrix[i][j] == 1) {
+            fill("#ffffcc");
+        }
+        rect(j * side, i * side, side, side);
+    }
+    if(data.count>40){
+        count=0;
+    }
+    //! Getting DOM objects (HTML elements)
+    
+
+
+    //! adding socket listener on "data" <-- name, after that fire 'drawCreatures' function 
+
+    // clickme.addEventListener('click',function(){
+    //     alert('dggdd')
+    // })
+
+    }
+}
+}
+
 }
--- server.js
+++ server.js
@@ -2,23 +2,61 @@
 //! Requiring modules  --  START
 var Grass = require("./modules/Grass.js");
 var GrassEater = require("./modules/GrassEater.js");
+var Bnapahpan = require("./modules/Bnapahpan.js");
+var Gishatich = require("./modules/Gishatich.js");
+var Kendanapahpan = require("./modules/Kendanapahpan.js");
+var Fire = require("./modules/Fire.js");
+var Balansapahpan = require("./modules/Balansapahpan.js");
 let random = require('./modules/random');
 //! Requiring modules  --  END
 
 
+// var i = 0;
+// function change() {
+//   var doc = document.getElementById("background");
+//   var color = ["#006600", "#009933", "brown", "green"];
+
+//   doc.style.backgroundColor = color[i];
+//   i = (i + 1) % color.length;
+// }
+// setInterval(change, 10000);
+
+
+
+guyn = "#006600";
+count = 0;
+setInterval(() => {
+    count++;
+    if (count > 40) {
+        count = 0;
+    }
+
+}, 10000)
+
+
 //! Setting global arrays  --  START
+KendanapahpanArr = [];
+BnapahpanArr = [];
+BalansapahpanArr = [];
 grassArr = [];
 grassEaterArr = [];
 matrix = [];
 grassHashiv = 0;
-grassEaterHashiv=0;
+grassEaterHashiv = 0;
+gishatichArr = [];
+fireArr = [];
+fireDur = 0;
+gishatichHashiv = 0;
+bnapahpanhashiv = 1;
+kendanapahpanhashiv = 1;
+balansapahpanhashiv = 1;
 //! Setting global arrays  -- END
 
 
 
 
 //! Creating MATRIX -- START
-function matrixGenerator(matrixSize, grass, grassEater, grassEaterEater, waterArr, fireArr) {
+function matrixGenerator(matrixSize, grass, grassEater, gishatich, bnapahpan, kendanapahpan, balansapahpan, fr) {
     for (let i = 0; i < matrixSize; i++) {
         matrix[i] = [];
         for (let o = 0; o < matrixSize; o++) {
@@ -35,23 +73,33 @@ function matrixGenerator(matrixSize, grass, grassEater, grassEaterEater, waterAr
         let customY = Math.floor(random(matrixSize));
         matrix[customY][customX] = 2;
     }
-    for (let i = 0; i < grassEaterEater; i++) {
+    for (let i = 0; i < gishatich; i++) {
         let customX = Math.floor(random(matrixSize));
         let customY = Math.floor(random(matrixSize));
         matrix[customY][customX] = 3;
     }
-    for (let i = 0; i < waterArr; i++) {
+    for (let i = 0; i < bnapahpan; i++) {
         let customX = Math.floor(random(matrixSize));
         let customY = Math.floor(random(matrixSize));
         matrix[customY][customX] = 4;
     }
-    for (let i = 0; i < fireArr; i++) {
+    for (let i = 0; i < kendanapahpan; i++) {
         let customX = Math.floor(random(matrixSize));
         let customY = Math.floor(random(matrixSize));
         matrix[customY][customX] = 5;
     }
+    for (let i = 0; i < balansapahpan; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 6;
+    }
+    for (let i = 0; i < fr; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 7;
+    }
 }
-matrixGenerator(20, 1, 1);
+matrixGenerator(30, 1, 1);
 //! Creating MATRIX -- END
 
 
@@ -66,10 +114,39 @@ app.get('/', function (req, res) {
     res.redirect('index.html');
 });
 server.listen(3000);
+count = 0;
+
+setInterval(function () {
+    count++;
+    if (count < 10) {
+        guyn = "#006600";
+
+    }
+    if (count >= 10 && count < 20) {
+        guyn = "#009933";
+    }
+    else if (count >= 20 && count < 30) {
+        guyn = "#cc9900";
+    }
+    else if (count >= 30 && count < 40) {
+        guyn = "#ffffcc";
+    }
+    else if (count >= 40) {
+        count = 0;
+    }
+}, 1000);
 //! SERVER STUFF END  --  END
-
-
-
+matrix[1][1] = 2;
+matrix[20][28] = 2;
+matrix[15][15] = 2;
+matrix[23][18] = 3;
+matrix[10][10] = 3;
+matrix[26][26] = 4;
+matrix[7][17] = 4;
+matrix[3][3] = 5;
+matrix[23][23] = 5;
+matrix[22][22] = 6;
+matrix[29][29] = 7;
 function creatingObjects() {
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
@@ -82,28 +159,123 @@ function creatingObjects() {
                 grassArr.push(grass);
                 grassHashiv++;
             }
+            if (matrix[y][x] == 3) {
+                var gishatich = new Gishatich(x, y);
+                gishatichArr.push(gishatich);
+                gishatichHashiv++;
+
+            }
+            if (matrix[y][x] == 4) {
+                var bnapahpan = new Bnapahpan(x, y);
+                BnapahpanArr.push(bnapahpan);
+
+            }
+            if (matrix[y][x] == 5) {
+                var kendanapahpan = new Kendanapahpan(x, y);
+                KendanapahpanArr.push(kendanapahpan);
+
+            }
+            if (matrix[y][x] == 6) {
+                var balansapahpan = new Balansapahpan(x, y);
+                BalansapahpanArr.push(balansapahpan);
+
+            }
+            // if (matrix[y][x] == 7) {
+            //     var fr = new Fire(x, y);
+            //     fireArr.push(fr);
+
+            // }
         }
     }
 }
+function end() {
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] == 1) {
+                fill(guyn);
+                rect(j * side, i * side, side, side);
+            } else if (matrix[i][j] == 2) {
+                fill('#acacac');
+                rect(j * side, i * side, side, side);
+            } else if (matrix[i][j] == 0) {
+                fill('#acacac');
+                rect(j * side, i * side, side, side);
+            } else if (matrix[i][j] == 3) {
+                fill('#acacac');
+                rect(j * side, i * side, side, side);
+            } else if (matrix[i][j] == 4) {
+                fill('#acacac');
+                rect(j * side, i * side, side, side);
+            } else if (matrix[i][j] == 5) {
+                fill('#acacac');
+                rect(j * side, i * side, side, side);
+            }
+            else if (matrix[i][j] == 6) {
+                fill('#acacac');
+                rect(j * side, i * side, side, side);
+            }
+            else if (matrix[i][j] == 7) {
+                fill('#acacac');
+                rect(j * side, i * side, side, side);
+            }
+        }
+    };
+}
 creatingObjects();
+
+
 
 function game() {
     if (grassArr[0] !== undefined) {
         for (var i in grassArr) {
-            grassArr[i].mul();
+            grassArr[i].bazmanal();
         }
     }
     if (grassEaterArr[0] !== undefined) {
         for (var i in grassEaterArr) {
-            grassEaterArr[i].utel();
+
+            grassEaterArr[i].eat();
+
         }
     }
+    if (gishatichArr[0] !== undefined) {
+        for (var i in gishatichArr) {
+            gishatichArr[i].eat();
+
+
+        }
+    }
+    if (grassArr.length <= 20) {
+        for (var i in BnapahpanArr) {
+            BnapahpanArr[i].sharjvel();
+        }
+    }
+    if (grassEaterArr.length <= 4) {
+        for (var i in KendanapahpanArr) {
+            KendanapahpanArr[i].sharjvel();
+        }
+    }
+    if (gishatichArr.length <= 1) {
+        for (var i in BalansapahpanArr) {
+            BalansapahpanArr[i].sharjvel();
+        }
+    }
+
+
+
+
+
 
     //! Object to send
     let sendData = {
         matrix: matrix,
         grassCounter: grassHashiv,
-        grassEaterCounter: grassEaterHashiv
+        grassEaterCounter: grassEaterHashiv,
+        gishatichcounter: gishatichHashiv,
+        count: count,
+        bnapahpanhashiv: bnapahpanhashiv,
+        kendanapahpanhashiv: kendanapahpanhashiv,
+        balansapahpanhashiv: balansapahpanhashiv
     }
 
     //! Send data over the socket to clients who listens "data"
@@ -112,6 +284,4 @@ function game() {
 
 }
 
-
-
-setInterval(game, 2000)
+setInterval(game, 300)
